9d0dade35cc994ef6cfc76222c23807e244e38ce
KunigiMohan <kmohanr5@gmail.com>
2021-11-07 20:20:25 +0530

    Added : Peek and pop methods from Stack

--- src/Stack.java
+++ src/Stack.java
@@ -2,10 +2,16 @@ public class Stack {
 
     public static void main(String[] args){
         LinkedList list= new LinkedList();
-        list.add(70);
+        list.add(70);                       //Adding elements to stack
         list.add(30);
         list.add(56);
         System.out.println("Stack of Given numbers is");
+        list.show();                                    //Calling show() method to show elements in Stack
+        System.out.println();
+        System.out.println("peek of the stack");
+        System.out.println(list.peek());        //Calling and printing peek of elements of Stack
+        list.pop();
+        System.out.println("Elements present after stack");
         list.show();
     }
 }
