56363afa87e23f0cac66c8ed9934b440365ef3b6
KunigiriMohan <90668633+KunigiriMohan@users.noreply.github.com>
2021-11-07 21:38:02 +0530

    Added : Creating Linked List of  56,70 and 30

--- src/Queue.java
+++ src/Queue.java
@@ -1,10 +1,10 @@
 public class Queue {
     public static void main(String[] args){
-        LinkedList list= new LinkedList();
-        list.enQueue(56);
+        LinkedList list= new LinkedList();          // creating object of Linked List
+        list.enQueue(56);                           //Inserting values in Linked list
         list.enQueue(30);
         list.enQueue(70);
         System.out.print("Elements in the Queue are :  ");
-        list.show();
+        list.show();                                //printing values in LInked List
     }
 }
