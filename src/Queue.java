c117d02f1617014571c590e9be11af0f81e85905
KunigiMohan <kmohanr5@gmail.com>
2021-11-11 14:18:14 +0530

    Added : Dequeue of queue from begining.

--- src/Queue.java
+++ src/Queue.java
@@ -2,9 +2,14 @@ public class Queue {
     public static void main(String[] args){
         LinkedList list= new LinkedList();
         list.enQueue(56);
-        list.enQueue(30);
+        list.enQueue(30);                       //pushing of elements to queue
         list.enQueue(70);
         System.out.print("Elements in the Queue are :  ");
-        list.show();
+        list.show();                                    //showing elements after pushing
+        //System.out.println(list.deQueue());
+        System.out.println();
+        list.deQueue();                                     //calling deQueue method to dequeue elements in queue
+        System.out.println("After deQueue of elements in Queue ");
+        list.show();                                    //printing elemnts in queue after dequeue
     }
 }
